83bfcd1deeab4fb175ffa8059dc3738e43e39a1a
Sofía Figueroa <sfigueroa@udesa.edu.ar>
2024-11-15 18:14:24 -0300

    pruebita

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home'
 
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home/>
     </View>
   );
 }
+
 
